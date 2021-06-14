89d28c098966be138b469df01eac1ffd09f2c623
mforets <mforets@gmail.com>
2021-06-14 13:56:46 -0300

    add clamped constant examples

--- clamped.jl
+++ clamped.jl
@@ -9,12 +9,13 @@ This model is taken from [1]. The theoretical derivations of the analytic soluti
 using ReachabilityAnalysis, LinearAlgebra, LazySets
 using ReachabilityAnalysis: solve, discretize
 using LazySets.Arrays
+using SparseArrays
 
 LazySets.set_ztol(Float64, 1e-15)
 LazySets.set_atol(Float64, 1e-15)
 LazySets.set_rtol(Float64, 1e-15)
 
-function clamped_bar(; N=1000, E=30e6, ρ=7.3e-4, A=1, L=200)
+function clamped_matrices(; N=1000, E=30e6, ρ=7.3e-4, A=1, L=200)
     ℓ = L / N
     K = (E * A / ℓ) * SymTridiagonal(fill(2.0, N), fill(-1.0, N))
     K[end, end] = E * A / ℓ
@@ -25,44 +26,60 @@ function clamped_bar(; N=1000, E=30e6, ρ=7.3e-4, A=1, L=200)
     return M, K
 end
 
-"""
-    clamped_free(; N)
-
-Free (no forcing term) instance of the clamped bar.
-"""
 function clamped_free(; N)
-    M, K = clamped_bar(N=N)
+    M, K = clamped_matrices(N=N)
     C = zeros(N, N) # no damping
     sys = SecondOrderLinearContinuousSystem(M, C, K)
 end
 
-"""
-    clamped_forced(; N, F=10e3, E=30e6, A=1)
-
-Forced (constant force `F`) instance of the clamped bar.
-"""
 function clamped_forced(; N, F=10e3, E=30e6, A=1)
-    M, K = clamped_bar(N=N)
+    M, K = clamped_matrices(N=N)
     C = zeros(N, N) # no damping
     F = vcat(zeros(N-1), F) # the right-most node is excited
     sys = SecondOrderAffineContinuousSystem(M, C, K, F)
 end
 
-# NOTE we can just use normalize(..)
-function clamped_canonical(; N)
+function clamped(; a=0.0, b=0.0, # ignored if damped = false
+                   N,            # number of elements
+                   homogeneize,  # flag to homogeneize the system
+                   damped)       # flag to consider damping C = a*K + b*M
+
     sys = clamped_forced(N=N)
     M = sys.M; K = sys.K; F = sys.b
 
-    A = [zeros(N, N)   Matrix(1.0I, N, N);
-         -inv(M)*K            zeros(N, N)];
+    invM = inv(M)    
+    ZN = zeros(N, N)
+    IN = Matrix(1.0I, N, N)
 
-    f = vcat(zeros(N), inv(M) * F);
-    return @system(x' = A*x + f)
+    if damped
+        C = a*K + b*M
+        A = [ZN           IN       ;
+            -invM*K       -invM*C]
+    
+    else
+        A = [ZN           IN ;
+            -invM*K       ZN]
+    end
+
+    f0 = vcat(zeros(N), invM * F)
+    
+    if homogeneize
+        n = 2N
+        Aext = zeros(n+1, n+1)
+        Aext[1:n, 1:n] .= A
+        Aext[1:n, n+1] .= f0
+        Aext = sparse(Aext)
+        return @system(x' = Aext*x)
+
+    else
+        return @system(x' = A*x + f0)
+    end
 end
 
 # "nominal" step size
 const dtn = 9.88e-7
 
+#=
 # problem instances
 s5 = clamped_forced(N=5);
 s10 = clamped_forced(N=10);
@@ -73,9 +90,12 @@ s100 = clamped_forced(N=100);
 s200 = clamped_forced(N=200);
 s500 = clamped_forced(N=500);
 s1000 = clamped_forced(N=1000);
+=#
+
+#=
 
 # singleton initial condition
-X0(n) = Singleton(zeros(2n))
+X0(N) = Singleton(zeros(2N))
 prob(s) = InitialValueProblem(s, X0(statedim(s)))
 
 # box initial condition
@@ -114,9 +134,10 @@ function solve_box(s; δ=dtn, NSTEPS=100, model=Forward(inv=false))
     sbox = solve(p, alg=alg, NSTEPS=NSTEPS, homogeneize=true)
     return sbox
 end
+=#
 
-# Ec. (4.181) libro Geradin & Rixen (2015)
-function _sol_analitica_clamped(x, t, Ncorte)
+# Ec. (4.181) ref Geradin & Rixen (2015)
+function _sol_analytic_clamped(x, t, Nmax)
 
     F = 10E3
     E = 30E6
@@ -137,7 +158,7 @@ function _sol_analitica_clamped(x, t, Ncorte)
     u = sin.( β * x ) * ( 1.0 .- cos.( β * c * t ) )
     v = sin.( β * x ) * β * c * sin.( β * c * t )
 
-    for s in 2:Ncorte
+    for s in 2:Nmax
         u .+= (-1)^(s-1)/(2*s-1)^2 * sin.( (2*s-1)* β * x ) * ( 1.0 .- cos.( (2*s-1)* β * c * t ) )
         v .+= (-1)^(s-1)/(2*s-1)^2 * sin.( (2*s-1)* β * x ) * ( (2*s-1)* β * c * sin.( (2*s-1)* β * c * t ) )
     end
